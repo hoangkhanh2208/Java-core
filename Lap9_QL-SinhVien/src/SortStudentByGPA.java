58c528f083d0fab5eb3a03ec6ed2e6cf81dafb15
Khanh <chuhoangkhanh00@gmail.com>
2021-04-15 18:26:47 +0700

    add ql sinhvien

---
+++ Lap9_QL-SinhVien/src/SortStudentByGPA.java
@@ -0,0 +1,16 @@
+import java.util.Comparator;
+
+/**
+ * SortStudentByGPA class
+ * 
+ * @author viettuts.vn
+ */
+public class SortStudentByGPA implements Comparator<Student> {
+    @Override
+    public int compare(Student student1, Student student2) {
+        if (student1.getGpa() > student2.getGpa()) {
+            return 1;
+        }
+        return -1;
+    }
+}
